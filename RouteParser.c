668d9eb1ba6ddf736a5c4ffd6fc0c4eedf4d9b09
Alicia Harman <aliciaharman2@gmail.com>
2024-11-03 01:57:17 -0600

    modified for new csv format

--- RouteParser.c
+++ RouteParser.c
@@ -10,7 +10,19 @@ typedef struct {
     char taw[6]; // time to arrive at work
     char tlw[6]; // time to leave work and go home
     int dw2h; // duration travel work to home
+    char tsw[6]; // desired time to start 8-hour workday
 } Route;
+
+void printRoutes(Route* r, int count) {
+    for (int i = 0; i < count; i++) {
+        printf("Route %d - Starting At %s\n",i,r[i].tsw);
+        printf("Time To Leave Home: %s\n",r[i].tlh);
+        printf("Duration To Work: %d\n",r[i].dh2w);
+        printf("Time To Arrive At Work: %s\n",r[i].taw);
+        printf("Time To Leave Work: %s\n",r[i].tlw);
+        printf("Duration To Home: %d\n",r[i].dw2h);
+    }
+}
 
 int main(int argc, char* argv[]) {
 
@@ -31,24 +43,34 @@ int main(int argc, char* argv[]) {
     int count = 0;
 
     while(fgets(line, MAX_LINE_LENGTH, fptr)) {
-        char t1[6];
-        int n; 
-        char t2[6]; 
+        char t1[6]; // time to leave starting location
+        int n;      // duration of travel to destination
+        char t2[6]; // time to arrive at destination
+        char t3[6]; // intended arrival/departure time
+        char c;     // either 's' for start time or 'e' for end time marker
 
         // parse the travel data
-        if (sscanf(line, "%5[^,],%d,%5[^,\n]", t1, &n, t2) == 3) {
+        if (sscanf(line, "%5[^,],%d,%5[^,],%5[^,],%c", t1, &n, t2, t3, &c) == 5) {
 
-            routes = realloc(routes, (count + 1) * sizeof(Route));
-            if (routes == NULL) {
-                perror("Memory allocation failed");
-                fclose(fptr);
-                return 1;
+            // add new Route to array if a start time
+            if (c == 's') {
+                // allocate memory for new Route
+                routes = realloc(routes, (count + 1) * sizeof(Route));
+                if (routes == NULL) {
+                    perror("Memory allocation failed");
+                    fclose(fptr);
+                    return 1;
+                }
+
+                // zero-initialize the newly allocated Route
+                memset(&routes[count], 0, sizeof(Route));
+
+                // store variables in Route object
+                strcpy(routes[count].tlh, t1);
+                routes[count].dh2w = n;
+                strcpy(routes[count].taw, t2);
+                strcpy(routes[count].tsw, t3);
             }
-
-            // store variables in Route object
-            strcpy(routes[count].tlh, t1);
-            routes[count].dh2w = n;
-            strcpy(routes[count].taw, t2);
 
             count++;
         } else {
@@ -59,9 +81,7 @@ int main(int argc, char* argv[]) {
     fclose(fptr);
 
     // print contents of array
-    for (int i = 0; i < count; i++) {
-        printf("%d | Start Time: %s | Number: %d | End Time: %s\n", i, routes[i].tlh, routes[i].dh2w, routes[i].taw);
-    }
+    printRoutes(routes, count);
 
     // deallocate memory for array
     free(routes);
